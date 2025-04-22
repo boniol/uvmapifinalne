a0d5d2aab7be687497bc589e868d9770be9f6f27
boniol <71459482+boniol@users.noreply.github.com>
2025-04-22 17:17:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
-import os, shutil
-from inference import separate_vocals
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
 import os
 import shutil
 from inference import separate_vocals
-import os
-import urllib.request
-from urllib.request import Request
+from urllib.request import Request, urlopen
 
 # Pobieranie tokenu z ENV
 token = os.environ.get("HUGGINGFACE_TOKEN")
@@ -23,24 +17,9 @@ if not os.path.exists(model_path):
     print("Downloading model from Hugging Face...")
     req = Request(model_url)
     req.add_header("Authorization", f"Bearer {token}")
-    with urllib.request.urlopen(req) as response, open(model_path, "wb") as out_file:
+    with urlopen(req) as response, open(model_path, "wb") as out_file:
         out_file.write(response.read())
     print("Model downloaded.")
-
-
-# 👇 TU dodaj kod pobierający model:
-import urllib.request
-
-model_url = "https://huggingface.co/Kuielito/UVR/resolve/main/UVR-MDX-NET-Inst_HQ_3.onnx"
-model_path = "models/UVR-MDX-NET-Inst_HQ_3.onnx"
-
-os.makedirs("models", exist_ok=True)
-
-if not os.path.exists(model_path):
-    print("Downloading model...")
-    urllib.request.urlretrieve(model_url, model_path)
-    print("Model downloaded.")
-
 
 app = FastAPI()
 
@@ -58,3 +37,4 @@ async def separate(file: UploadFile = File(...)):
     separate_vocals(input_path, output_path)
 
     return FileResponse(output_path, media_type="audio/wav", filename="instrumental.wav")
+
